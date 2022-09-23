10f9776c750fd2abbc8afdfdb18be0a750704da8
Matthew Cook <matthewcook13987@gmail.com>
2022-09-23 13:22:08 -0500

    adds CPU choice logic

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+function getCPUchoice() {
+    switch(getRandomInt(3))
+    {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissor";
+        }
+}
+console.log("CPU Chose: " + getCPUchoice())
+
+function game(getCPUchoice, playerChoice){
+
+}
