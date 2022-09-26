ae4a1cb4c92f8089a9037977ee2e2dd7221ff00f
Matthew Cook <matthewcook13987@gmail.com>
2022-09-26 13:28:12 -0500

    begins adding score tracking logic

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -25,28 +25,47 @@ function playRound(playerSelection, computerSelection) {
 
     //logic for playerSelection 'Rock"
     } else if (playerSelection == "rock" && computerSelection == "scissors"){
+        playerScore++
         return "You win! Rock beats Scissors!"
     } else if (playerSelection == "rock" && computerSelection == "paper"){
+        computerScore++
         return "You lose! Paper beats Rock!"
+        
     
     //logic for playerSelection "Paper"
     } else if (playerSelection == "paper" && computerSelection == "scissors"){
+        computerScore++
         return "You Lose! Scissors beats Paper!"
     } else if (playerSelection == "paper" && computerSelection == "rock"){
+        playerScore++
         return "You Win! Paper beats rock!"
+        
     
 
     //logic for playerSelection "Scissors"
     } else if (playerSelection == "scissors" && computerSelection == "paper"){
+         playerScore++
          return "You Win! Scissors beats Paper!"
     } else if (playerSelection == "scissors" && computerSelection == "rock"){
-    return "You Lose! Rock beats Scissors!"
+        computerScore++
+        return "You Lose! Rock beats Scissors!"
+    
     }   
 }
 
+let playerScore = 0
+let computerScore = 0
+const playerSelection = "rock";
+const computerSelection = getCPUchoice();
 
-   
-  const playerSelection = "rock";
-  const computerSelection = getCPUchoice();
-  console.log("You chose: " + playerSelection + ", computer chose: " + computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+function game(){
+    while (playerScore || computerScore < 5) {
+        playRound(playerSelection, computerSelection)
+        return "Score is: \n p1:" + playerScore + "\n p2:" + computerScore
+    }
+    
+}
+
+//   console.log("You chose: " + playerSelection + ", computer chose: " + computerSelection);
+//   console.log(playRound(playerSelection, computerSelection));
+  
